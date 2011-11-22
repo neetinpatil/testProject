e81c26e3742c29a985b1242803c4617f47981a6b
Nitin <patil.nitin@cmcltd.com>
2011-11-22 12:21:47 +0530

    yyyy

--- Remoteproject/src/HelloWorld.java
+++ Remoteproject/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	
 	public static void main(String args[]){
 
-               system.out.println("uuuuuuuuuuuuuuuuuuuuu");
+               System.out.println("uuuuuuuuuuuuuuuuuuuuu");
 		
 	}
 
